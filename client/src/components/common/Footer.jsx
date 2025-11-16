34d83e6fe5511c57a8987fcd67d086ebf3eebadb
jashanx <jashanpreet1605@gmail.com>
2025-11-16 20:23:25 +0530

    modified NotificationCenter.jsx

--- client/src/components/common/Footer.jsx
+++ client/src/components/common/Footer.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useTheme } from '../../contexts/ThemeContext.jsx';
-
-// Simple, minimal footer to avoid complexity and ensure reliability
 const Footer = () => {
   const { isDarkMode } = useTheme();
   const currentYear = new Date().getFullYear();
